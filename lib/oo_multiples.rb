b89afd739a2ffb0a45fcae9a82435d45ab8edbec
Drew Price <drewprice@users.noreply.github.com>
2015-10-12 11:26:20 -0400

    Add oo solution

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...limit).select { |n| multiple_of?([3, 5], n) }
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(&:+)
+  end
+
+  private
+
+  def multiple_of?(divisors, candidate)
+    divisors.any? { |d| candidate % d == 0 }
+  end
+end
