78f032b754e667b0bb683d8878f13ba8cdf54028
Drew Price <drewprice@users.noreply.github.com>
2015-10-12 11:02:18 -0400

    Add procedural solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,11 @@
-# Enter your procedural solution here!
+def multiple_of_3_or_5?(n)
+  n % 3 == 0 || n % 5 == 0
+end
+
+def collect_multiples(limit)
+  (1...limit).select { |n| multiple_of_3_or_5?(n) }
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(&:+)
+end
